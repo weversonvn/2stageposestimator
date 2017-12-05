6d44ae0245df0efc905c0cb1012b5280a3c60f97
weversonvn <weverson-nascimento@hotmail.com>
2017-12-05 13:52:40 -0300

    inclusão de licença

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 __author__ = "Weverson Nascimento"
 __credits__ = ["Weverson Nascimento"]
-__license__ = "GPL"
+__license__ = "apache-2.0"
 __version__ = "1.0.1"
 __maintainer__ = "Weverson Nascimento"
 __email__ = "weverson@ufpa.br"
@@ -13,6 +13,24 @@ __status__ = "Production"
     Date last modified: 25/11/2017
     Python Version: 2.7
 '''
+
+
+'''
+   Copyright 2017 Weverson Nascimento
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
 
 import numpy as np
 import cv2

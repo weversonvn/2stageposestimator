9156eb55cca70d2726879c9e25157eef7fbd9689
Weverson Nascimento <weverson@localhost.localdomain>
2017-12-22 17:50:07 -0300

    adicionada busca dentro do dataset de imagens

--- kpcasub.py
+++ kpcasub.py
@@ -1,21 +1,6 @@
-__author__ = "Weverson Nascimento"
-__credits__ = ["Weverson Nascimento"]
-__license__ = "apache-2.0"
-__version__ = "0.1"
-__maintainer__ = "Weverson Nascimento"
-__email__ = "weverson@ufpa.br"
-__status__ = "Production"
+"""
+   kpcasub
 
-'''
-    File name: kpcasub.py
-    Author: Weverson Nascimento
-    Date created: 14/12/2017
-    Date last modified: 17/12/2017
-    Python Version: 2.7
-'''
-
-
-'''
    Copyright 2017 Weverson Nascimento
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,12 +14,34 @@ __status__ = "Production"
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+"""
+
+__author__ = "Weverson Nascimento"
+__credits__ = ["Weverson Nascimento"]
+__license__ = "apache-2.0"
+__version__ = "0.1"
+__maintainer__ = "Weverson Nascimento"
+__email__ = "weverson@ufpa.br"
+__status__ = "Production"
+
+'''
+    File name: kpcasub.py
+    Author: Weverson Nascimento
+    Date created: 14/12/2017
+    Date last modified: 20/12/2017
+    Python Version: 2.7
 '''
 
 
 from sklearn.decomposition import KernelPCA
 
-def projecaokpca(array, escalas, rotacoes):
+def projecaokpca(X):
+    kpca = KernelPCA(n_components=30, kernel="rbf", n_jobs=-1)
+    coeficientes = np.empty([escalas*rotacoes,55,55])
     for escala in range(escalas):
         for rotacao in range(rotacoes):
-            np.real(array[escala*rotacoes+rotacao])
+            # para cada rotacao, cria prototipos
+            real = np.real(X[escala*rotacoes+rotacao])
+            coeficientes[escala*rotacoes+rotacao]= kpca.fit_transform(real)
+
+    return coeficientes
--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-__author__ = "Weverson Nascimento"
-__credits__ = ["Weverson Nascimento"]
-__license__ = "apache-2.0"
-__version__ = "0.1"
-__maintainer__ = "Weverson Nascimento"
-__email__ = "weverson@ufpa.br"
-__status__ = "Production"
+"""
+   2stageposestimator
 
-'''
-    File name: main.py
-    Author: Weverson Nascimento
-    Date created: 24/09/2017
-    Date last modified: 18/12/2017
-    Python Version: 2.7
-'''
-
-
-'''
    Copyright 2017 Weverson Nascimento
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -29,28 +14,84 @@ __status__ = "Production"
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+"""
+
+__author__ = "Weverson Nascimento"
+__credits__ = ["Weverson Nascimento"]
+__license__ = "apache-2.0"
+__version__ = "0.1"
+__maintainer__ = "Weverson Nascimento"
+__email__ = "weverson@ufpa.br"
+__status__ = "Production"
+
+'''
+    File name: main.py
+    Author: Weverson Nascimento
+    Date created: 24/09/2017
+    Date last modified: 22/12/2017
+    Python Version: 2.7
 '''
 
 
-import os
+
+#import os
+import sys
 import numpy as np
+
 import cv2
 import matplotlib.pyplot as plt 
 import bob.ip.gabor
 from sklearn.decomposition import KernelPCA
+
 from pre import pre
+from kpcasub import projecaokpca
+#print(__doc__)
 
-imagem = pre('/home/weverson/Downloads/HeadPoseImageDatabase/Front/personne01146+0+0.jpg') # imagem de entrada
 
-def treino(imagem):
+def wavextract(imagem):
     # extracao de coeficientes com wavelet de Gabor
-    gwt = bob.ip.gabor.Transform(number_of_scales = 6) # cria a transformada
+    escalas = 6
+    gwt = bob.ip.gabor.Transform(number_of_scales = escalas) # cria a transformada
+    rotacoes = gwt.number_of_directions
     trafo_image = gwt(imagem) # aplica a transformada na imagem
+    for escala in range(escalas):
+        for rotacao in range(rotacoes):
+            real = np.real(X[escala*rotacoes+rotacao]) # extrai parte real
+            wav_coefs = np.reshape(real,3685) # vetoriza a matriz
+    return wav_coefs
 
-    # projecao no subespaco KPCA
-    # aplica o KPCA nos coeficientes reais da wavelet
-    #kpca = KernelPCA(kernel="rbf", fit_inverse_transform=True, gamma=10) # cria a transformada de projecao
-    #X_kpca = kpca.fit_transform(trafo_image) # aplica o KPCA
 
-#def teste():
+def readimg(caminho):
+    pessoa = 1
+    serie = 1
+    for pessoa in range(1,16):
+        for serie in range(1,3):
+            for i in range(93):
+                panplus = ""
+                tiltplus = ""
+                if i == 0:
+                    tilt = -90
+                    pan = 0
+                elif i == 92:
+                    tilt = 90
+                    pan = 0
+                else :
+                    pan = ((i-1) % 13-6)*15
+                    tilt = ((i-1) / 13-3)*15
+                    if (abs(tilt) == 45):
+                        tilt = tilt / abs(tilt)*60
+                if pan >= 0:
+                    panplus = "+"
+                if tilt >= 0:
+                    tiltplus = "+"
+                imgfile = caminho + 'Person' + str(pessoa).zfill(2) + '/person' + str(pessoa).zfill(2) + str(serie) + str(i).zfill(2) + tiltplus + str(tilt) + panplus + str(pan) + '.jpg'
+                if i == 1:
+                    print imgfile
+                imagem = pre(imgfile)
+                wav_coefs = wavextract(imagem)
+
+    
+if __name__ == '__main__':
+    caminho = sys.argv[1]
+    readimg(caminho)
 
